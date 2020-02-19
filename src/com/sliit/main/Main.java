2f61a5fdb783d4ac91a09783dc6519776344e2d4
SathKav <sathsaranika@gmail.com>
2020-02-19 00:36:13 -0800

    new hello

--- src/com/sliit/main/Main.java
+++ src/com/sliit/main/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("This is my first project at SLIIT 4 ");
-System.out.println("Hello world");
+System.out.println("new Hello world");
 	}
 
 }
